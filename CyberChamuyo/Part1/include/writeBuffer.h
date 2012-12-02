97fbb4ab703557cbe8dc75fa4e25201a421e91c7
marianowilliner <marianowilliner@gmail.com>
2012-12-02 03:45:32 +0000

    Fuck SVN

--- CyberChamuyo/Part1/include/writeBuffer.h
+++ CyberChamuyo/Part1/include/writeBuffer.h
@@ -1,66 +1,104 @@
 #ifndef WRITEBUFFER_H_
 #define WRITEBUFFER_H_
 
-#include <fstream>
+#include <queue>
 
-#include "buffer.h"
-
-template<class T> class WriteBuffer : public Buffer<T> {
+template<class File,class Record> class WriteBuffer {
 private:
+	std::queue<Record> queue;
+
+	File outputFile;
+
+	unsigned int maxSize;
+
+	//Requerido para poder usar registros de longitud fija.
+	unsigned int recordSize;
+
+	std::queue<Record>& getQueue();
+
+	File& getFile();
+
+	unsigned int getMaxSize() const;
+
+	void setMaxSize(unsigned int maxSize);
+
+	unsigned int getRecordSize() const;
+
+	void setRecordSize(unsigned int recordSize);
+
+	//Metodo para cargar en el buffer los proximos registros del archivo
 	void flush();
+
 public:
-	WriteBuffer();
+	//Constructor para usar con archivos de registros de longitud variable.
+	WriteBuffer(unsigned int maxSize);
 
-	WriteBuffer(std::string filePath, unsigned int maxSize);
+	WriteBuffer(unsigned int maxSize, unsigned int recordSize);
 
-	void initialize(std::string filePath, unsigned int maxSize);
+	void initialize(std::string outFilepath);
 
-	void finalize();
+	void putRecord(const Record& record);
 
-	void putRecord(const T& record);
+	bool empty();
 
+	//Destructor.
 	~WriteBuffer();
 };
 
-template<class T> WriteBuffer<T>::WriteBuffer() {
+template<class File,class Record> WriteBuffer<File,Record>::WriteBuffer(unsigned int maxSize) {
+	this->maxSize = maxSize;
 }
 
-template<class T> WriteBuffer<T>::WriteBuffer(std::string filePath, unsigned int maxSize):Buffer<T>(maxSize) {
-	this->getFile().open(filePath.c_str(),std::iostream::out | std::iostream::binary);
+template<class File,class Record> WriteBuffer<File,Record>::WriteBuffer(unsigned int maxSize, unsigned int recordSize) {
+	this->maxSize = maxSize;
+	this->recordSize = recordSize;
 }
 
-template<class T> void WriteBuffer<T>::putRecord(const T& record) {
-	this->push(record);
-	if (this->size() == this->getMaxSize()) {
+template<class File,class Record> void WriteBuffer<File,Record>::initialize(std::string outFilepath) {
+	this->flush();
+	this->getFile().close();
+	this->getFile().open(outFilepath);
+}
+
+template<class File,class Record> std::queue<Record>& WriteBuffer<File,Record>::getQueue() {
+	return this->queue;
+}
+
+template<class File,class Record> File& WriteBuffer<File,Record>::getFile() {
+	return this->outputFile;
+}
+
+template<class File,class Record> unsigned int WriteBuffer<File,Record>::getMaxSize() const {
+	return this->maxSize;
+}
+
+template<class File,class Record> void WriteBuffer<File,Record>::setMaxSize(unsigned int maxSize) {
+	this->maxSize = maxSize;
+}
+
+template<class File,class Record> void WriteBuffer<File,Record>::flush() {
+	Record record;
+
+	while (!this->empty()) {
+		this->getFile().putRecord(this->getQueue().front());
+		this->getQueue().pop();
+	}
+}
+
+template<class File,class Record> void WriteBuffer<File,Record>::putRecord(const Record& record) {
+	this->getQueue().push(record);
+	if (this->getQueue().size() == this->getMaxSize()) {
 		this->flush();
 	}
 }
 
-template<class T> void WriteBuffer<T>::flush() {
-	Record* record = new T();
-
-	while (this->size() > 0) {
-		*record = this->getBuffer().front();
-		this->getFile() << record->serialize() << std::endl;
-		this->pop();
-	}
-
-	delete record;
+template<class File,class Record> bool WriteBuffer<File,Record>::empty() {
+	return this->getQueue().empty();
 }
 
-template<class T> void WriteBuffer<T>::initialize(std::string filePath, unsigned int maxSize) {
-	this->setMaxSize(maxSize);
-	this->finalize();
-	this->getFile().open(filePath.c_str(),std::iostream::out | std::iostream::binary);
-}
-
-template<class T> void WriteBuffer<T>::finalize() {
+template<class File,class Record> WriteBuffer<File,Record>::~WriteBuffer() {
 	this->flush();
 	this->getFile().close();
-}
-
-template<class T> WriteBuffer<T>::~WriteBuffer() {
-	this->finalize();
 }
 
 #endif /* WRITEBUFFER_H_ */
